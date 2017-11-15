200f7ce3bf0434aabaafd5b69e6a9a5a39a3c0db
lost-in-Code-au <reece.c.jones@gmail.com>
2017-11-15 20:26:43 +0000

    qwry

---
+++ webApp.js
@@ -0,0 +1,14 @@
+//Firebase app code!
+<script src="https://www.gstatic.com/firebasejs/4.6.2/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDhsH4FXxdlN9UegLr0_P2UDuOXp-WySk0",
+    authDomain: "react-native-firebase-st-d0137.firebaseapp.com",
+    databaseURL: "https://react-native-firebase-st-d0137.firebaseio.com",
+    projectId: "react-native-firebase-st-d0137",
+    storageBucket: "react-native-firebase-st-d0137.appspot.com",
+    messagingSenderId: "723325076292"
+  }
+  firebase.initializeApp(config)
+</script>
